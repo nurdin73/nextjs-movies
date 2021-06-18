8117928512cbb3b5485723f6de51af8c4a6a7d26
nurdin73 <nurdin.reverse73@gmail.com>
2021-06-18 13:45:27 +0700

    update

--- pages/detail/tv/[tv_id]/seasons/index.js
+++ pages/detail/tv/[tv_id]/seasons/index.js
@@ -1,10 +1,22 @@
 import { Fragment } from "react"
 import slugify from "slugify"
+import Head from 'next/head'
 import BannerHeading from "../../../../../components/BannerHeading"
 import Card from "../../../../../components/Card"
 function Seasons({ tvSeason }) {
     return (
         <Fragment>
+            <Head>
+                <title>{tvSeason.name || tvSeason.original_name} seasons list | LUX movie rating</title>
+                <meta name="description" content={tvSeason.overview} />
+                <meta property="og:title" id="titleOg" content={ tvSeason.name || tvSeason.original_name + " seasons list | LUX movie rating" } />
+                <meta property="og:description" id="descOg" content={ tvSeason.overview } />
+                <meta property="og:site_name" content="LUX | online movie ratings" />
+                <meta property="og:type" content="website" />
+                <meta property="og:image" content={`https://image.tmdb.org/t/p/w500${tvSeason.poster_path}`} />
+                <meta property="og:image:width" content="245" />
+                <meta property="og:image:height" content="71" />
+            </Head>
             <BannerHeading 
                 poster={tvSeason.poster_path}
                 title={tvSeason.name || tvSeason.original_name}
@@ -13,7 +25,7 @@ function Seasons({ tvSeason }) {
                 tagline={tvSeason.tagline}
             />
             <div className="px-3 md:container md:mx-auto pt-3 pb-3">
-                <ul className="grid grid-cols-1 md:grid-cols-6 gap-3 mt-2">
+                <ul className="grid grid-cols-2 md:grid-cols-6 gap-3 mt-2">
                     {tvSeason.seasons.map((season, key) => {
                         return (
                             <Card 
