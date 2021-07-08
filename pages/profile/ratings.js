bcc3063fc02e9f791630f7236b49295c4e684431
nurdin73 <nurdin.reverse73@gmail.com>
2021-07-08 13:23:36 +0700

    update

--- pages/profile/ratings.js
+++ pages/profile/ratings.js
@@ -6,6 +6,7 @@ import Image from "next/image"
 import slugify from "slugify"
 import DateStr from "../../components/DateStr"
 import Link from "next/link"
+import { pagination } from "../../components/Pagination"
 function Ratings() { 
     const router = useRouter()
     const [data, setData] = useState(null)
@@ -13,6 +14,13 @@ function Ratings() {
     const [aliasTab, setAliasTab] = useState('movie')
     const [account, setAccount] = useState(null)
     const [session, setSession] = useState(null)
+
+    // paginate
+    const [totalResult, setTotalResult] = useState(null)
+    const [pages, setPage] = useState(null)
+    const [paginate, setPaginate] = useState(null)
+    const [prevDisabled, setPrevDisabled] = useState(null)
+    const [nextDisabled, setNextDisabled] = useState(null)
 
     useEffect(() => {
         const sessionId = Cookies.get('session_id')
@@ -26,9 +34,31 @@ function Ratings() {
             const req = await fetch(`https://api.themoviedb.org/3/account/${myAccount.id}/rated/movies?api_key=${process.env.API_KEY}&session_id=${sessionId}&sort_by=created_at.desc`)
             const res = await req.json()
             setData(res)
+            setTotalResult(res.total_results)
+            setPage(res.page)
+            const paginations = pagination(res.total_results, res.page)
+            setPaginate(paginations)
+            setPrevDisabled(paginations.currentPage === 1 ? true : false)
+            setNextDisabled(paginations.currentPage === paginations.totalPages ? true : false)
         }
         getFav()
     }, [setData])
+
+    const handlePaginate = (pageNumber = 1) => () => {
+        window.scrollTo({top: 0, behavior: 'smooth'});
+        async function getData(url = "") {
+            const res = await fetch(url)
+            const resp = await res.json()
+            setData(resp)
+            setPage(resp.page)
+            setTotalResult(resp.total_results)
+            const paginations = pagination(resp.total_results, resp.page)
+            setPaginate(paginations)
+            setPrevDisabled(paginations.currentPage === 1 ? true : false)
+            setNextDisabled(paginations.currentPage === paginations.totalPages ? true : false)
+        }
+        getData(`https://api.themoviedb.org/3/account/${account.id}/rated/${tab}?api_key=${process.env.API_KEY}&session_id=${session}&sort_by=created_at.desc&page=${pageNumber}`)
+    }
 
     const handleTabShow = (nameTab = "") => async () => {
         setTab(nameTab)
@@ -40,6 +70,12 @@ function Ratings() {
         const req = await fetch(`https://api.themoviedb.org/3/account/${account.id}/rated/${nameTab}?api_key=${process.env.API_KEY}&session_id=${session}&sort_by=created_at.desc`)
         const res = await req.json()
         setData(res)
+        setTotalResult(res.total_results)
+        setPage(res.page)
+        const paginations = pagination(res.total_results, res.page)
+        setPaginate(paginations)
+        setPrevDisabled(paginations.currentPage === 1 ? true : false)
+        setNextDisabled(paginations.currentPage === paginations.totalPages ? true : false)
     }
     return (
         <Fragment>
@@ -48,7 +84,7 @@ function Ratings() {
             </Head>
             <div className="md:container md:mx-auto px-3">
                 <h1 className="text-2xl text-yellow-500 uppercase md:py-3 py-1 border-b-2 border-gray-700 md:mb-3 mb-1">Ratings</h1>
-                <ul className="flex space-x-2 whitespace-nowrap mb-3">
+                <ul className="flex space-x-2 whitespace-nowrap mb-3 mt-3">
                     <li onClick={handleTabShow('movies')} className={`${tab === "movies" ? "bg-yellow-500 text-gray-900" : "md:bg-gray-900 bg-gray-800"} px-3 cursor-pointer hover:bg-yellow-500 hover:text-gray-900 transition duration-300 py-1 text-md text-gray-500 rounded`}>Movies</li>
                     <li onClick={handleTabShow('tv')} className={`${tab === "tv" ? "bg-yellow-500 text-gray-900" : "md:bg-gray-900 bg-gray-800"} px-3 cursor-pointer hover:bg-yellow-500 hover:text-gray-900 transition duration-300 py-1 text-md text-gray-500 rounded`}>Tv Show</li>
                 </ul>
@@ -62,6 +98,36 @@ function Ratings() {
                     : <h1 className="uppercase text-xl col-span-8 text-yellow-500">Object not found</h1>
                     }
                 </ul>
+                <div className="flex justify-center mt-4">
+                    <ul className="flex justify-start items-center rounded overflow-hidden">
+                        <li key="prev">
+                            <button onClick={handlePaginate(pages > 1 ? pages - 1  :pages)} disabled={prevDisabled} className={`${prevDisabled === true ? 'opacity-50 cursor-not-allowed' : ""} md:px-3 md:py-1 px-1.5 py-1 md:text-lg text-sm hover:bg-gray-600 cursor-pointer bg-gray-700 text-gray-300`}>Prev</button>
+                        </li>
+                        {pages > 490 ? 
+                            <li key="...">
+                                <button disabled className="opacity-50 focus:outline-none cursor-not-allowed md:px-3 md:py-1 px-1.5 py-1 md:text-lg text-sm hover:bg-gray-600 bg-gray-700 text-gray-300">...</button>
+                            </li>
+                            : ""
+                        } 
+                        {paginate?.pages.map((page, key) => {
+                            const active = pages === page ? "bg-gray-900 hover:bg-opacity-70" : "bg-gray-700 hover:bg-gray-600"
+                            return (
+                                <li key={key}>
+                                    <button onClick={handlePaginate(page)} className={`md:px-3 md:py-1 px-1.5 py-1 md:text-lg text-sm ${active} cursor-pointer focus:outline-none text-gray-300`}>{page}</button>
+                                </li>
+                            )
+                        })}
+                        {pages < 490 ? 
+                            <li key="...">
+                                <button disabled className="opacity-50 cursor-not-allowed md:px-3 md:py-1 px-1.5 py-1 md:text-lg text-sm hover:bg-gray-600 focus:outline-none bg-gray-700 text-gray-300">...</button>
+                            </li>
+                            : ""
+                        } 
+                        <li key="next">
+                            <button  onClick={handlePaginate(pages !== paginate?.totalPages ? pages + 1  :pages)} disabled={nextDisabled} className={`${nextDisabled === true ? 'opacity-50 cursor-not-allowed' : ""} md:px-3 md:py-1 px-1.5 py-1 md:text-lg text-sm focus:outline-none hover:bg-gray-600 cursor-pointer bg-gray-700 text-gray-300`}>Next</button>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </Fragment>
     )
--- pages/profile/watchlists.js
+++ pages/profile/watchlists.js
@@ -4,6 +4,7 @@ import {useRouter} from 'next/router'
 import Cookies from "js-cookie"
 import Card from '../../components/Card'
 import slugify from "slugify"
+import { pagination } from "../../components/Pagination"
 function Watchlists() {  
 
     const router = useRouter()
@@ -12,6 +13,13 @@ function Watchlists() {
     const [aliasTab, setAliasTab] = useState('movie')
     const [account, setAccount] = useState(null)
     const [session, setSession] = useState(null)
+
+    // paginate
+    const [totalResult, setTotalResult] = useState(null)
+    const [pages, setPage] = useState(null)
+    const [paginate, setPaginate] = useState(null)
+    const [prevDisabled, setPrevDisabled] = useState(null)
+    const [nextDisabled, setNextDisabled] = useState(null)
 
     useEffect(() => {
         const sessionId = Cookies.get('session_id')
@@ -25,9 +33,30 @@ function Watchlists() {
             const req = await fetch(`https://api.themoviedb.org/3/account/${myAccount.id}/watchlist/movies?api_key=${process.env.API_KEY}&session_id=${sessionId}&sort_by=created_at.desc`)
             const res = await req.json()
             setData(res)
+            setPage(res.page)
+            const paginations = pagination(res.total_results, res.page)
+            setPaginate(paginations)
+            setPrevDisabled(paginations.currentPage === 1 ? true : false)
+            setNextDisabled(paginations.currentPage === paginations.totalPages ? true : false)
         }
         getFav()
     }, [setData])
+
+    const handlePaginate = (pageNumber = 1) => () => {
+        window.scrollTo({top: 0, behavior: 'smooth'});
+        async function getData(url = "") {
+            const res = await fetch(url)
+            const resp = await res.json()
+            setData(resp)
+            setPage(resp.page)
+            setTotalResult(resp.total_results)
+            const paginations = pagination(resp.total_results, resp.page)
+            setPaginate(paginations)
+            setPrevDisabled(paginations.currentPage === 1 ? true : false)
+            setNextDisabled(paginations.currentPage === paginations.totalPages ? true : false)
+        }
+        getData(`https://api.themoviedb.org/3/account/${account.id}/watchlist/${tab}?api_key=${process.env.API_KEY}&session_id=${session}&sort_by=created_at.desc&page=${pageNumber}`)
+    }
 
     const handleTabShow = (nameTab = "") => async () => {
         setTab(nameTab)
@@ -39,6 +68,12 @@ function Watchlists() {
         const req = await fetch(`https://api.themoviedb.org/3/account/${account.id}/watchlist/${nameTab}?api_key=${process.env.API_KEY}&session_id=${session}&sort_by=created_at.desc`)
         const res = await req.json()
         setData(res)
+        setPage(res.page)
+        setTotalResult(resp.total_results)
+        const paginations = pagination(resp.total_results, resp.page)
+        setPaginate(paginations)
+        setPrevDisabled(paginations.currentPage === 1 ? true : false)
+        setNextDisabled(paginations.currentPage === paginations.totalPages ? true : false)
     }
 
     return (
@@ -48,7 +83,7 @@ function Watchlists() {
             </Head>
             <div className="md:container md:mx-auto px-3">
                 <h1 className="text-2xl text-yellow-500 uppercase md:py-3 py-1 border-b-2 border-gray-700 md:mb-3 mb-1">Watchlists</h1>
-                <ul className="flex space-x-2 whitespace-nowrap mb-3">
+                <ul className="flex space-x-2 whitespace-nowrap mb-3 mt-3">
                     <li onClick={handleTabShow('movies')} className={`${tab === "movies" ? "bg-yellow-500 text-gray-900" : "md:bg-gray-900 bg-gray-800"} px-3 cursor-pointer hover:bg-yellow-500 hover:text-gray-900 transition duration-300 py-1 text-md text-gray-500 rounded`}>Movies</li>
                     <li onClick={handleTabShow('tv')} className={`${tab === "tv" ? "bg-yellow-500 text-gray-900" : "md:bg-gray-900 bg-gray-800"} px-3 cursor-pointer hover:bg-yellow-500 hover:text-gray-900 transition duration-300 py-1 text-md text-gray-500 rounded`}>Tv Show</li>
                 </ul>
@@ -62,6 +97,36 @@ function Watchlists() {
                     : ""
                     }
                 </ul>
+                <div className="flex justify-center mt-4">
+                    <ul className="flex justify-start items-center rounded overflow-hidden">
+                        <li key="prev">
+                            <button onClick={handlePaginate(pages > 1 ? pages - 1  :pages)} disabled={prevDisabled} className={`${prevDisabled === true ? 'opacity-50 cursor-not-allowed' : ""} md:px-3 md:py-1 px-1.5 py-1 md:text-lg text-sm hover:bg-gray-600 cursor-pointer bg-gray-700 text-gray-300`}>Prev</button>
+                        </li>
+                        {pages > 490 ? 
+                            <li key="...">
+                                <button disabled className="opacity-50 focus:outline-none cursor-not-allowed md:px-3 md:py-1 px-1.5 py-1 md:text-lg text-sm hover:bg-gray-600 bg-gray-700 text-gray-300">...</button>
+                            </li>
+                            : ""
+                        } 
+                        {paginate?.pages.map((page, key) => {
+                            const active = pages === page ? "bg-gray-900 hover:bg-opacity-70" : "bg-gray-700 hover:bg-gray-600"
+                            return (
+                                <li key={key}>
+                                    <button onClick={handlePaginate(page)} className={`md:px-3 md:py-1 px-1.5 py-1 md:text-lg text-sm ${active} cursor-pointer focus:outline-none text-gray-300`}>{page}</button>
+                                </li>
+                            )
+                        })}
+                        {pages < 490 ? 
+                            <li key="...">
+                                <button disabled className="opacity-50 cursor-not-allowed md:px-3 md:py-1 px-1.5 py-1 md:text-lg text-sm hover:bg-gray-600 focus:outline-none bg-gray-700 text-gray-300">...</button>
+                            </li>
+                            : ""
+                        } 
+                        <li key="next">
+                            <button  onClick={handlePaginate(pages !== paginate?.totalPages ? pages + 1  :pages)} disabled={nextDisabled} className={`${nextDisabled === true ? 'opacity-50 cursor-not-allowed' : ""} md:px-3 md:py-1 px-1.5 py-1 md:text-lg text-sm focus:outline-none hover:bg-gray-600 cursor-pointer bg-gray-700 text-gray-300`}>Next</button>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </Fragment>
     )
